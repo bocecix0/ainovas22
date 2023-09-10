dcc3687fef0c5ef0d58ec5a1d771d01b877cf2e4
bocecix0 <100798486+bocecix0@users.noreply.github.com>
2023-09-10 15:55:33 +0300

    landing-hero.tsx

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,15 +12,15 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
+        <h1>En İyi E-ticaret Yapay Zeka Aracı</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <TypewriterComponent
             options={{
               strings: [
-                "Chatbot.",
-                "Photo Generation.",
-                "Blog Writing.",
-                "Mail Writing."
+                "Seotool Aracı.",
+                "Ürün görseli oluştur.",
+                "Açıklama oluştur.",
+                "Başlık oluştur."
               ],
               autoStart: true,
               loop: true,
@@ -29,12 +29,12 @@ export const LandingHero = () => {
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 10x faster.
+        Yapay zekayı kullanarak 10 kat daha fazla satış yapın.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
-            Start Generating For Free
+            Ücretsiz kullanmaya başla
           </Button>
         </Link>
       </div>
